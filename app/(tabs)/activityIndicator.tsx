b6f2c958c5ba5b4f8e0c6834d6d0c75b0a2a0fbe
rahmat.bizhan <rahmat.bizhan@afghan-wireless.com>
2025-08-27 09:48:20 +0430

    activity Indicator

---
+++ app/(tabs)/activityIndicator.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    ActivityIndicator,
+    StyleSheet, Text,
+    View
+} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator
+       size="large" 
+       color="lightgreen"/>
+      <Text style={styles.text}>Loading...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text : {
+    marginTop : 10,
+  }
+  
+});
+export default App;

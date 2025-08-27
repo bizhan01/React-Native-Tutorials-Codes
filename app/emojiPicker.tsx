b513809ce3b314df017a767011aeafdb7675bc7b
rahmat.bizhan <rahmat.bizhan@afghan-wireless.com>
2025-08-27 10:37:55 +0430

    EmojiPicker App

---
+++ app/emojiPicker.tsx
@@ -0,0 +1,88 @@
+// Importing required dependencies
+import React, { useState } from "react";
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
+} from "react-native";
+import EmojiSelector, { Categories } from "react-native-emoji-selector";
+
+// Function component to select emoji
+const EmojiPicker = () => {
+    const [showPicker, setShowPicker] = useState(false);
+    const [selectedEmoji, setSelectedEmoji] = useState("");
+
+    // Function to handle the selected emoji
+    const handleEmojiSelect = (emoji) => {
+        setSelectedEmoji(emoji);
+        setShowPicker(false);
+    };
+
+    return (
+        <View>
+            <TouchableOpacity
+                onPress={() => setShowPicker(true)}
+                style={styles.pickerButton}
+            >
+                <Text style={styles.buttonText}>
+                    Open Emoji Picker
+                </Text>
+            </TouchableOpacity>
+            <View style={{ marginTop: 10 }}>
+                {selectedEmoji !== "" && (
+                    <Text style={{ fontSize: 20 }}>
+                        Selected Emoji: {selectedEmoji}
+                    </Text>
+                )}
+            </View>
+
+            // To display the emoji picker and search
+            {showPicker && (
+                <EmojiSelector
+                    onEmojiSelected={handleEmojiSelect}
+                    category={Categories.all}
+                    showTabs={true}
+                    showSearchBar={true}
+                    showHistory={true}
+                    columns={10}
+                    placeholder="Search emoji..."
+                />
+            )}
+        </View>
+    );
+};
+
+// App component that will be exported
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <EmojiPicker />
+        </View>
+    );
+};
+
+// Adding styles to the application
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f5f5f5",
+    },
+    pickerButton: {
+        backgroundColor: "#007bff",
+        borderRadius: 8,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+});
+
+// Export main component
+export default App;

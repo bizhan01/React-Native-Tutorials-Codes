658973301faf3e8651cd92742d5e0087e3492097
rahmat.bizhan <rahmat.bizhan@afghan-wireless.com>
2025-08-27 09:54:56 +0430

    Button

---
+++ app/(tabs)/button.jsx
@@ -0,0 +1,44 @@
+// Import necessary modules from React and React Native
+import { useState } from 'react';
+// Import StyleSheet, Text, View, and Button components from React Native
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+// Define the main App component as the default export
+export default function App() {
+  // Declare a state variable 'count' with an initial value of 0 and a function 'setcount' to update it
+  const [count, setcount] = useState(0);
+
+  // Function to increment the count value by 1
+  const changeCount = () => {
+    setcount(count + 1);
+  };
+
+  // Render the UI
+  return (
+    <View style={styles.container}>
+      {/* Display the current count value */}
+      <Text style={styles.text}>{count}</Text>
+      {/* Button to trigger the changeCount function when pressed */}
+      <Button
+        title={"Click Me"}
+        onPress={changeCount}
+      />
+    </View>
+  );
+}
+
+// Define styles for the components
+const styles = StyleSheet.create({
+  // Style for the main container
+  container: {
+    flex: 1, // Take up the full height of the screen
+    backgroundColor: '#fff', // Set background color to white
+    alignItems: 'center', // Center items horizontally
+    justifyContent: 'center', // Center items vertically
+  },
+  // Style for the text displaying the count
+  text: {
+    fontSize: 40, // Set font size to 40
+    marginBottom: 30, // Add margin below the text
+  }
+});

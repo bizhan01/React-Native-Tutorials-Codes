1cfeffeb058ace8bafe1fdcaaae766f6f2c1e1f4
rahmat.bizhan <rahmat.bizhan@afghan-wireless.com>
2025-08-27 09:57:56 +0430

    Dumb Components

---
+++ app/(tabs)/dumbComponent.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+// Creating a functional component
+const GFG = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Geeksforgeeks</Text>
+    </View>
+  );
+};
+
+// Creating a stylesheet
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "green",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 25,
+    backgroundColor: "white",
+    margin: 10,
+    borderRadius: 5,
+  },
+});
+
+// Creating the main App component
+export default function App() {
+  return (
+    // Using the GFG component multiple times
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <GFG />
+      <GFG />
+      <GFG />
+      <GFG />
+    </View>
+  );
+}
